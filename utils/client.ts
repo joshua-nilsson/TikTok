78e2b908a42aeaa1d81b01178998b55d2fea5a9c
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2022-07-21 02:04:33 -0600

    GitHub Sync With Local Environment

--- utils/client.ts
+++ utils/client.ts
@@ -1,9 +1,9 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
-  dataset: 'production',
-  apiVersion: '2022-03-10',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2022-07-20',
   useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
